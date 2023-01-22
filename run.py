da4d672ea037fc2a7e70490d1e306099b9e28a4f
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-01-22 09:25:51 +0000

    define function to connect age to right training program

--- run.py
+++ run.py
@@ -20,6 +20,31 @@ def get_age():
 
     data_str = input("Enter your data here: ")
     print(f"The data provided is {data_str}")
+    validate_age(get_age)
+
+def validate_age(value):
+    """
+    Inside the try, convert user answer to integers. Create ValueError if age isn't written in numbers.
+    """
+    try:
+        if len(value) != 1:
+            raise ValueError(
+                f"Only one value is required not {len(value)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+def connect_training():
+    if get_age <= 20:
+        teenager = SHEET.worksheet('teenager')
+    elif get_age <= 35: 
+         adult = SHEET.worksheet('adult')
+    elif get_age <= 50:
+        mid_life = SHEET.worksheet('mid_life')
+    elif get_age <= 70:
+        elder = SHEET.worksheet('elder')
+    else:
+        senior = SHEET.worksheet('senior')
 
 
 get_age()
