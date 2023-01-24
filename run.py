84db921601556d9997c82e7110bfbfdf3106ab0e
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-01-24 16:23:17 +0000

    add tomorrows training schedule in worksheet

--- run.py
+++ run.py
@@ -76,10 +76,9 @@ def get_result():
 
 def update_worksheet(data, worksheet):
     """
-    Receives a list of integers to be inserted into a worksheet
-    Update the relevant worksheet with the data provided
+    Update tomorrows training program to the worksheet.
     """
-    print(f"Updating {worksheet} worksheet...\n")
+    print(f"Updating {worksheet} tomorrows training program...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
